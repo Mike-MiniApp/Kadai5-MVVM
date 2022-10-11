352ad33b52eb408873a206be0323607bfdd703e4
mikekondo <foad5969@gmail.com>
2022-10-12 07:27:29 +0900

    add: Inputs,Outputs,Type

--- Kadai5-MVVM/ViewModels/ViewModel.swift
+++ Kadai5-MVVM/ViewModels/ViewModel.swift
@@ -6,3 +6,58 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
+import RxRelay
+
+// MARK: - Inputs
+protocol ViewModelInputs {
+    var number1TextFieldObservable: Observable<String> { get }
+    var number2TextFieldObservable: Observable<String> { get }
+    var calcButtonTapObservable: Observable<String> { get }
+}
+
+// MARK: - Outputs
+protocol ViewModelOutputs {
+    var calcResultPublishRelay: PublishRelay<String> { get }
+}
+
+// MARK: - Type
+protocol ViewModelType {
+    var inputs: ViewModelInputs { get }
+    var outputs: ViewModelOutputs { get }
+}
+
+class ViewModel: ViewModelInputs, ViewModelOutputs {
+
+    // MARK: - Inputs
+    var number1TextFieldObservable: RxSwift.Observable<String>
+    var number2TextFieldObservable: RxSwift.Observable<String>
+    var calcButtonTapObservable: RxSwift.Observable<String>
+
+    // MARK: - Outputs
+    var calcResultPublishRelay = RxRelay.PublishRelay<String>()
+
+    init(number1TextFieldObservable: Observable<String>,
+         number2TextFieldObservable: Observable<String>, calcButtonTapObservable: Observable<String>){
+        self.number1TextFieldObservable = number1TextFieldObservable
+        self.number2TextFieldObservable = number2TextFieldObservable
+        self.calcButtonTapObservable = calcButtonTapObservable
+        setupBindings()
+    }
+
+    private func setupBindings() {
+        
+    }
+
+
+}
+
+// MARK: - ViewModelType
+extension ViewModel: ViewModelType {
+    var inputs: ViewModelInputs { return self }
+
+    var outputs: ViewModelOutputs { return self }
+}
+
+
--- Kadai5-MVVM/Views/ViewController.swift
+++ Kadai5-MVVM/Views/ViewController.swift
@@ -6,14 +6,20 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+import RxRelay
 
 class ViewController: UIViewController {
 
+    @IBOutlet private weak var number1TextField: UITextField!
+    @IBOutlet private weak var number2TextField: UITextField!
+    @IBOutlet private weak var calcButton: UIButton!
+    @IBOutlet private weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
 
